946ffdd8fec1f215edb81b7b03c8d3800ddc0a8f
patz paul <pathefister@gmail.com && git config --global user.name patzpaulgit config --global user.email pathefister@gmail.com>
2023-09-13 19:34:49 +0300

    initiating commit

---
+++ client/src/components/AIPicker.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import CustomButton from "./CustomButton";
+
+const AIPicker = ({ prompt, setPrompt, generatingImg, handleSubmit }) => {
+  return (
+    <div className="aipicker-container absolute ml-5">
+      <textarea
+        placeholder="Ask AI..."
+        rows={5}
+        value={prompt}
+        onChange={(e) => setPrompt(e.target.value)}
+        className="aipicker-textarea"
+      />
+      <div className=" mt-3 flex flex-wrap gap-2">
+        {generatingImg ? (
+          <CustomButton
+            type="outline"
+            title="Asking AI..."
+            customStyles="text-xs"
+          />
+        ) : (
+          <>
+            <CustomButton
+              type="outline"
+              title="logo"
+              handleClick={() => handleSubmit("logo")}
+              customStyles="text-xs"
+            />
+            <CustomButton
+              type="filled"
+              title="AI Full"
+              handleClick={() => handleSubmit("full")}
+              customStyles="text-xs"
+            />
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default AIPicker;
